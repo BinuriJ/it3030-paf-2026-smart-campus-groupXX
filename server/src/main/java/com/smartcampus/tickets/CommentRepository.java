bc48d339e71a9c1a9e4f7b8b05bab1760adbd4e8
BinuriJ <binurijayasinghe@gmail.com>
2026-04-22 18:17:31 +0530

    refactor: restructure backend to layered architecture and add comments

--- server/src/main/java/com/smartcampus/tickets/CommentRepository.java
+++
@@ -1,9 +0,0 @@
-package com.smartcampus.tickets;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-import java.util.List;
-
-public interface CommentRepository extends MongoRepository<Comment, String> {
-    List<Comment> findByTicketId(String ticketId);
-    void deleteByTicketId(String ticketId);
-}
